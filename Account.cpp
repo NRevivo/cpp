2b84efdfd2eeb2b0e560b433bb326fba1d8b405b
Noam Revivo <noam.revivo.1@gmail.com>
2024-07-19 12:09:57 +0300

    third

--- Account.cpp
+++ Account.cpp
@@ -1,1 +1,14 @@
 #include "Account.h"
+
+Account::Account() : m_transactionList(nullptr) , m_numberOfTransaction(0), m_persons(nullptr), 
+                    m_totalPersons(0), m_accountNumber(0), m_balance(0.0){}
+
+Account::Account( Person** persons, int count, double balance ) : m_transactionList(nullptr) , m_numberOfTransaction(0), 
+                    m_totalPersons(1), m_accountNumber(0), m_balance(balance) 
+{
+m_persons = new Person* [count];
+for (int i = 0; i < count; ++i) 
+{
+        m_persons[i] = new Person(*persons[i]);
+}
+}
--- Person.cpp
+++ Person.cpp
@@ -1,1 +1,62 @@
 #include "Person.h"
+
+Person::Person() : m_name(nullptr), m_id(0){}
+Person::Person(const char* name, int id) : m_id(id)
+{
+    if (name)
+    {
+        m_name = new char[strlen(name) + 1];
+        strcpy (m_name, name);
+    }
+    else
+    {
+        m_name = nullptr;
+    }
+     
+}
+Person::Person(const Person& other) : m_id(other.m_id) 
+{
+    if (other.m_name) 
+    {
+        m_name = new char[strlen(other.m_name) + 1];
+        strcpy(m_name, other.m_name);
+    } 
+    else 
+    {
+        m_name = nullptr;
+    }
+}
+
+Person::~Person() {
+    delete[] m_name;
+}
+
+void Person::SetName(const char* newName) 
+{
+    delete[] m_name; 
+    if (newName)
+    {
+        m_name = new char[strlen(newName) + 1];
+        strcpy(m_name, newName);
+    } 
+    else 
+    {
+        m_name = nullptr;
+    }
+}
+
+void Person::SetId(int newId)
+{
+    m_id = newId;
+}
+
+char* Person::GetName() const
+{
+    return m_name;
+}
+
+int Person::GetId() const
+{
+    return m_id;
+}
+
